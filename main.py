05f9a1bd0c81ac705f3964b1858a75b61b3814e4
madhu1995-oss <54890298+madhu1995-oss@users.noreply.github.com>
2019-11-19 02:11:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer
+import nltk
+import re
+from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+model =load("model.h5")
+def predictions(text):
+  clean = re.sub(r'[^ a-z A-Z 0-9]', " ", text)
+  test_word = word_tokenize(clean)
+  test_word = [w.lower() for w in test_word]
+  test_ls = word_tokenizer.texts_to_sequences(test_word)
+  print(test_word)
+  #Check for unknown words
+  if [] in test_ls:
+    test_ls = list(filter(None, test_ls))
+    
+  test_ls = np.array(test_ls).reshape(1, len(test_ls))
+ 
+  x = padding_doc(test_ls, max_length)
+  
+  pred = model.predict_proba(x)
+  
+  
+  return pred
+  def get_final_output(pred, classes):
+  predictions = pred[0]
+ 
+  classes = np.array(classes)
+  ids = np.argsort(-predictions)
+  classes = classes[ids]
+  predictions = -np.sort(-predictions)
+ 
+  for i in range(pred.shape[1]):
+    print("%s has confidence = %s" % (classes[i], (predictions[i])))
+text = str(input())
+pred = predictions(text)
+get_final_output(pred, unique_intent)
